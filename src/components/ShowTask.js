85c4de26374c9759b3c912543da9895c91a2fa34
Firman <109858426+firmangits@users.noreply.github.com>
2023-04-11 14:06:44 +0700

    overriding vulnarabelity package json

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,5 +1,5 @@
-export const ShowTask = ({taskList, setTaskList, task, setTask}) => {
-   
+export const ShowTask = ({ taskList, setTaskList, task, setTask }) => {
+
     // const handleEdit = () => {
     //     console.log("object");
     // }
@@ -9,29 +9,32 @@ export const ShowTask = ({taskList, setTaskList, task, setTask}) => {
     //     setTaskList(updatedTaskList);
     // }
 
-  return (
-    <section className="showTask">
-        <div className="head">
-            <div>
-                <span className="title">Todo</span>
-                <span className="count">{taskList.length}</span>
+    return (
+        <section className="showTask">
+            <div className="head">
+                <div>
+                    <span className="title">Todo</span>
+                    <span className="count">{taskList.length}</span>
+                </div>
+                <button className="clearAll" onClick={() => setTaskList([])}>Clear All</button>
             </div>
-            <button className="clearAll" onClick={() => setTaskList([])}>Clear All</button>
-        </div>
-        <ul>
-            {
-                taskList.map((task) => (
-                    <li key={task.id}>
-                        <p>
-                            <span className="name">{task.name}</span>
-                            <span className="time">{task.time}</span>
-                        </p>
-                        <i onClick={handleEdit(task.id)} className="bi bi-pencil-square"></i>
-                        <i onClick={handleDelete(task.id)} className="bi bi-trash"></i>
-                    </li>
-                ))
-            }
-        </ul>
-    </section>
-  )
+            <ul>
+                {
+                    taskList.map((task) => (
+                        <li key={task.id}>
+                            <p>
+                                <span className="name">{task.name}</span>
+                                <span className="time">{task.time}</span>
+                            </p>
+                            {/* <i onClick={handleEdit(task.id)} className="bi bi-pencil-square"></i>
+                        <i onClick={handleDelete(task.id)} className="bi bi-trash"></i> */}
+
+                            <i className="bi bi-pencil-square"></i>
+                            <i className="bi bi-trash"></i>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
+    )
 }
